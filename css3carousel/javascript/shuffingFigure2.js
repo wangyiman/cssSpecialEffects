0117c2b77c588544435c27bb628a29fdd18f82a3
wangyiman <962932619@qq.com>
2018-12-09 21:58:26 +0800

    add second version

---
+++ css3carousel/javascript/shuffingFigure2.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+    let images = document.getElementsByTagName('img');
+    let imageParent = document.getElementsByClassName('img_list')[0];
+    let i = 0;
+
+    let btn = document.getElementsByClassName('circle');
+
+    Array.prototype.forEach.call(images, function(img) {
+        pictureResize(img);
+    });
+
+    function showPicture(index) {
+        let timer = setInterval(function() {
+            if(typeof(index) === 'number') {
+                i = index;
+            }
+
+            if((i)*200 === 1200) {
+                imageParent.style.transform = `translate3d(${-1400}px, 0, 0)`;
+                imageParent.style.transitionDuration = `${0}s`;
+                imageParent.style.transform = `translate3d(${-200}px, 0, 0)`;
+                i = 1;
+                return;
+            }
+
+            imageParent.style.transitionDuration = `${1}s`;
+            imageParent.style.transform = `translate3d(${-200*(i+1)}px, 0, 0)`;
+            if(i === 5) {
+                selectedBtn(0);
+            } else {
+                selectedBtn(i);
+            }
+            i++;
+        }, 1000);
+        return timer;
+    }
+
+    function selectedBtn(i) {
+        for(let num = 0;num < btn.length;num++) {
+            if(num === i) {
+                btn[num].className = 'circle selected';
+            } else {
+                btn[num].className = 'circle';
+            }
+        }
+    }
+
+    let timer = showPicture();
+
+    for(var j = 0;j < btn.length;j++) {
+        btn[j].onmouseenter = (function(index) {
+            return function() {
+                clearInterval(timer);
+                imageParent.style.transform = `translate3d(${-200*(index + 1)}px, 0, 0)`;
+                selectedBtn(index);
+                i = index + 1;
+            }
+        })(j);
+        btn[j].onmouseleave = (function(index) {
+            return function() {
+                timer = showPicture();
+                imageParent.style.transform = `translate3d(${-200*(index + 1)}px, 0, 0)`;
+            }
+        })(j);
+    }
+}
--- css3carousel/readme.md
+++ css3carousel/readme.md
@@ -6,4 +6,7 @@ https://github.com/LYZ0106/Carousel-figure
 https://github.com/chenqing/carousel
 
 #目前存在的问题
-轮播了一轮后，到第二轮播放的时候，第一张图播放后会有1s钟的停顿，后续需要优化
+1.轮播了一轮后，到第二轮播放的时候，第一张图播放后会有1s钟的停顿，后续需要优化
+2.需要加上前进后退的按钮
+
+overflow：hidden后新增了元素，怎么都显示不出来
