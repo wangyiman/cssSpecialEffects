a5b20caa0e0c7448bc9f71576feb2244b94f208f
wangyiman <962932619@qq.com>
2018-12-13 21:33:24 +0800

    add new feature for carousel

--- css3carousel/javascript/shuffingFigure2.js
+++ css3carousel/javascript/shuffingFigure2.js
@@ -1,7 +1,9 @@
 window.onload = function() {
     let images = document.getElementsByTagName('img');
     let imageParent = document.getElementsByClassName('img_list')[0];
-    let i = 0;
+    let picture_list = document.getElementsByClassName('picture_list')[0];
+    let i = 1;
+    let animationing = false;
 
     let btn = document.getElementsByClassName('circle');
 
@@ -9,28 +11,80 @@ window.onload = function() {
         pictureResize(img);
     });
 
-    function showPicture(index) {
-        let timer = setInterval(function() {
-            if(typeof(index) === 'number') {
-                i = index;
-            }
+    function showPicture() {
+        if(animationing) return;
 
-            if((i)*200 === 1200) {
-                imageParent.style.transform = `translate3d(${-1400}px, 0, 0)`;
-                imageParent.style.transitionDuration = `${0}s`;
+        animationing = true;
+        imageParent.style.transitionDuration = `${0.5}s`;
+        if(i === 5) {
+            imageParent.style.transform = `translate3d(${-1200}px, 0, 0)`;
+            var innerTimer = setInterval(function() {
+                imageParent.style.transitionDuration = '0.0s';//important
                 imageParent.style.transform = `translate3d(${-200}px, 0, 0)`;
+                animationing = false;
+                clearInterval(innerTimer);
                 i = 1;
-                return;
-            }
-
-            imageParent.style.transitionDuration = `${1}s`;
+            }, 1000);
+            //此处要大于transition的时间1s才能看起来流畅
+            //否则transition还没做完，图片就切换了
+        } else {
             imageParent.style.transform = `translate3d(${-200*(i+1)}px, 0, 0)`;
-            if(i === 5) {
-                selectedBtn(0);
-            } else {
-                selectedBtn(i);
-            }
             i++;
+            animationing = false;
+        }
+    }
+
+    function next() {
+        if(animationing) return; //important
+        if(i === 5) {
+            selectedBtn(0);
+        } else {
+            selectedBtn(i);
+        }
+        showPicture();
+    }
+
+    function showPicture_prev() {
+        if(animationing) return;
+
+        animationing = true;
+        imageParent.style.transitionDuration = `${0.5}s`;
+        if(i < 0) {
+            imageParent.style.transform = `translate3d(${0}px, 0, 0)`;
+            var innerTimer = setInterval(function() {
+                imageParent.style.transitionDuration = '0.0s';//important
+                imageParent.style.transform = `translate3d(${-1000}px, 0, 0)`;
+                animationing = false;
+                clearInterval(innerTimer);
+            }, 1000);
+            //此处要大于transition的时间才能看起来流畅
+            //否则transition还没做完，图片就切换了
+        } else {
+            imageParent.style.transform = `translate3d(${-200*(i+1)}px, 0, 0)`;
+            animationing = false;
+        }
+    }
+    function prev() {
+        if(animationing) return; //important
+
+        if(i > 0) {
+            i--;
+            selectedBtn(i);
+            showPicture_prev();
+        } else if(i===0){
+            //而i === 0的时候走的是下面的逻辑
+            //切换保持在第一处 -200 ——》0
+            //按钮选择的却是4
+            i--;
+            selectedBtn(4);
+            showPicture_prev();
+            i = 4;
+        }
+    }
+
+    function defaultShowPicture(index) {
+        let timer = setInterval(function() {
+            next();
         }, 1000);
         return timer;
     }
@@ -45,7 +99,7 @@ window.onload = function() {
         }
     }
 
-    let timer = showPicture();
+    let timer = defaultShowPicture();
 
     for(var j = 0;j < btn.length;j++) {
         btn[j].onmouseenter = (function(index) {
@@ -56,11 +110,35 @@ window.onload = function() {
                 i = index + 1;
             }
         })(j);
-        btn[j].onmouseleave = (function(index) {
+        btn[j].onmouseleave = (function() {
             return function() {
-                timer = showPicture();
-                imageParent.style.transform = `translate3d(${-200*(index + 1)}px, 0, 0)`;
+                timer = defaultShowPicture();
             }
-        })(j);
+        })();
     }
+
+    let arrow1 = document.getElementsByClassName('arrow1')[0];
+    let arrow2 = document.getElementsByClassName('arrow2')[0];
+    arrow1.addEventListener('click', () => {
+        clearInterval(timer);
+        next();
+    }, false);
+    arrow2.addEventListener('click', () => {
+        clearInterval(timer);
+        prev();
+    }, false);
+
+    arrow1.addEventListener('mouseleave', () => {
+        clearInterval(timer);
+        timer = defaultShowPicture();
+    }, false);
+    arrow2.addEventListener('mouseleave', () => {
+        clearInterval(timer);
+        timer = defaultShowPicture();
+    }, false);
+
+    picture_list.addEventListener('mouseleave', () => {
+        clearInterval(timer);
+        timer = defaultShowPicture();
+    }, false);
 }
